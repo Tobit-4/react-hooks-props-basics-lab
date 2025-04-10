8ff830b7b96658d23b073c446dc0fd15e967b25a
Fidel <fidel.kipkoech@student.moringaschool.com>
2025-04-10 20:33:54 +0300

    made changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,30 @@
 import React from "react";
 
-function About() {
+function About(props) {
+function display(){
+  if(props.bio && props.bio !== ""){
+    return <p>{props.bio}</p>
+  } else {
+    return null;
+  }
+}
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {display()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
     </div>
   );
+  function Links (props){
+    return (
+    <div>
+      <h3>Links</h3>
+      <a href={props.github}>{props.github}</a>
+      <a href={props.linkedin}>{props.linkedin}</a>
+    </div>
+    );
+  }
 }
 
 export default About;
